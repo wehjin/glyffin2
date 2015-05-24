67ef1b1a09e9e40300e9edf88c243099541100e4
Jeffrey Yu <wehjin@gmail.com>
2015-05-25 01:24:39 +0200

    Draw 3 points.

--- index.js
+++ index.js
@@ -3,15 +3,25 @@
  */
 ///<reference path="webglbook.d.ts" />
 var VSHADER_SOURCE = 'attribute vec4 a_Position;\n' + 'void main(){\n' + '  gl_Position = a_Position;\n' + '  gl_PointSize = 10.0;\n' + '}\n';
-var FSHADER_SOURCE = 'void main(){\n' + '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' + '}\n';
+var FSHADER_SOURCE = 'void main(){\n' + '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + '}\n';
 function main() {
     var canvas = document.getElementById('webgl');
     var gl = getWebGLContext(canvas);
     initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    var n = initVertexBuffers(gl);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, n);
+}
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    var n = 3;
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    return n;
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ var VSHADER_SOURCE : string =
 
 var FSHADER_SOURCE : string =
     'void main(){\n' +
-    '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
+    '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
     '}\n';
 
 function main() {
@@ -22,10 +22,22 @@ function main() {
 
     initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-
+    var n = initVertexBuffers(gl);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, n);
+}
+
+function initVertexBuffers(gl : WebGLBookContext) : number {
+    var vertices : Float32Array = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    var n : number = 3;
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    return n;
 }
