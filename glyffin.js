da90a1629822c5657271c419f362f498a541f81d
Jeffrey Yu <wehjin@gmail.com>
2015-05-26 03:05:59 +0200

    Add Show class.

--- glyffin.js
+++ glyffin.js
@@ -1,9 +1,41 @@
 /**
  * Created by wehjin on 5/24/15.
  */
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User";
-document.body.innerHTML = greeter(user);
+var RelBounds = (function () {
+    function RelBounds() {
+    }
+    return RelBounds;
+})();
+var GlAudience = (function () {
+    function GlAudience() {
+    }
+    GlAudience.prototype.addRel = function (bounds) {
+        return null;
+    };
+    return GlAudience;
+})();
+var Show = (function () {
+    function Show(onShow) {
+        this.onShow = onShow;
+    }
+    Show.create = function (onShow) {
+        return new Show(onShow);
+    };
+    Show.prototype.show = function (audience, producer) {
+        var curtains = [];
+        this.onShow.onShow({
+            addCurtain: function (curtain) {
+                curtains.push(curtain);
+            }
+        });
+        return {
+            close: function () {
+                while (curtains.length > 0) {
+                    curtains.pop().close();
+                }
+            }
+        };
+    };
+    return Show;
+})();
 //# sourceMappingURL=glyffin.js.map
--- glyffin.ts
+++ glyffin.ts
@@ -1,10 +1,66 @@
 /**
  * Created by wehjin on 5/24/15.
  */
-function greeter(person : string) {
-    return "Hello, " + person;
+
+class RelBounds {
+    left : number;
+    right : number;
+    top : number;
+    bottom : number;
 }
 
-var user = "Jane User"
+interface Rel {
+    remove();
+}
 
-document.body.innerHTML = greeter(user);
+interface Audience {
+    addRel(bounds : RelBounds):Rel;
+}
+
+class GlAudience implements Audience {
+
+    addRel(bounds : RelBounds) : Rel {
+        return null;
+    }
+}
+
+interface Producer<T> {
+    onResult():T;
+}
+
+interface Curtain {
+    close();
+}
+interface Director {
+    addCurtain(curtain : Curtain);
+}
+
+interface OnShow {
+    onShow(director : Director);
+}
+
+
+class Show<T> {
+    static create<U>(onShow : OnShow) : Show<U> {
+        return new Show<U>(onShow);
+    }
+
+    constructor(private onShow : OnShow) {
+    }
+
+    show(audience : Audience, producer : Producer<T>) : Curtain {
+        var curtains : Curtain[] = [];
+        this.onShow.onShow({
+            addCurtain(curtain : Curtain) {
+                curtains.push(curtain);
+            }
+        });
+        return {
+            close() {
+                while (curtains.length > 0) {
+                    curtains.pop().close();
+                }
+            }
+        }
+    }
+}
