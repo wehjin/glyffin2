ceabb8bb9907002156df0daff6858e5772651c70
Jeffrey Yu <wehjin@gmail.com>
2015-06-21 13:43:38 -0700

    Cycle through items.

--- feed.ts
+++ feed.ts
@@ -23,16 +23,30 @@ function main() {
     var yNewsUri = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Frss.news.yahoo.com%2Frss%2Ftopstories%22&format=json&callback=";
     Rx.httpGet(hNewsUri).subscribe((response : Rx.HttpResponse)=> {
         var items = response.json['query']['results']['item'];
-        var item = items[0];
-        var title = item['title'] + " - " + item['link'];
-        var cell = Glyffin.colorPath([0, 1]).addNearMajor(1,
-            Glyffin.asciiMultiLine(3, title).pad(metrics.readHeight * 2,
-                metrics.readHeight)
-        ).limitHeight(metrics.tapHeight * 2, 0);
+        var index = 0;
+        var presentation;
 
-        var app = Glyffin.colorPath([0, 0])
-            .addNearMajor(1, cell);
-        app.present(metrics, glAudience);
+        function refresh() {
+            if (presentation) {
+                presentation.end();
+            }
+            var item = items[index % items.length];
+            var title = item['title'] + " - " + item['link'];
+            var cell = Glyffin.colorPath([0, 1]).addNearMajor(1,
+                Glyffin.asciiMultiLine(3, title).pad(metrics.readHeight * 2,
+                    metrics.readHeight)
+            ).limitHeight(metrics.tapHeight * 2, 0);
+
+            var app = Glyffin.colorPath([0, 0])
+                .addNearMajor(1, cell)
+                .addTopMajor(metrics.tapHeight, Glyffin.button());
+            presentation = app.present(metrics, glAudience, ()=> {
+                index++;
+                refresh();
+            });
+        }
+
+        refresh();
     });
 }
 
