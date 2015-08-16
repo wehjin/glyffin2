597f3241c5012b38301c2c58d99932c56086403c
Jeffrey Yu <wehjin@gmail.com>
2015-08-16 16:26:01 -0700

    Add input textures.

--- demo/frametest.js
+++ demo/frametest.js
@@ -28,48 +28,72 @@ function main() {
     gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
     gl.clearColor(0, 0, 0, 1);
     gl.enable(gl.DEPTH_TEST);
-    var vertexShader = "" + "attribute vec2 a_TexCoord;\n" + "attribute vec2 a_Position;\n" + "varying vec2 v_TexCoord;\n" + "void main(void) {\n" + "  gl_PointSize = 2.0;\n" + "  gl_Position = vec4(a_Position, 0.0, 1.0);\n" + "  v_TexCoord = a_TexCoord;\n" + "}\n";
-    var fragmentShader = "" + "precision mediump float;\n" + "uniform sampler2D u_Sampler;\n" + "varying vec2 v_TexCoord;\n" + "void main(void) {\n" + "  gl_FragColor = texture2D(u_Sampler, gl_PointCoord);\n" + "}\n";
+    var identity = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+    var fractionAndAlignment = [.5, .5, .5, .5];
+    var vertexShader = "" + "precision mediump float;\n" + "uniform sampler2D u_parentMatrixA;\n" + "uniform sampler2D u_parentMatrixB;\n" + "uniform sampler2D u_parentMatrixC;\n" + "uniform sampler2D u_parentMatrixD;\n" + "uniform sampler2D u_fractionAndAlignment;\n" + "attribute vec2 a_TexCoord;\n" + "attribute vec2 a_Position;\n" + "varying vec2 v_TexCoord;\n" + "varying mat4 v_modelToWorld;\n" + "const mat4 ident = mat4(1.0);\n" + "void main(void) {\n" + "  vec4 parentMatrixA = texture2D(u_parentMatrixA, a_TexCoord);\n" + "  vec4 parentMatrixB = texture2D(u_parentMatrixB, a_TexCoord);\n" + "  vec4 parentMatrixC = texture2D(u_parentMatrixC, a_TexCoord);\n" + "  vec4 parentMatrixD = texture2D(u_parentMatrixD, a_TexCoord);\n" + "  mat4 parentMatrix = mat4(parentMatrixA, parentMatrixB, parentMatrixC, parentMatrixD);\n" + "  vec4 fractionAndAlignment = texture2D(u_fractionAndAlignment, a_TexCoord);\n" + "  vec2 fraction = fractionAndAlignment.xy;\n" + "  vec2 alignment = fractionAndAlignment.zw;\n" + "  v_modelToWorld = parentMatrix * mat4(ident[0]*fraction.x,ident[1]*fraction.y, ident[2], ident[3]);\n" + "  gl_PointSize = 2.0;\n" + "  gl_Position = vec4(a_Position, 0.0, 1.0);\n" + "}\n";
+    var fragmentShader = "" + "precision mediump float;\n" + "varying mat4 v_modelToWorld;\n" + "void main(void) {\n" + "  if (gl_PointCoord.x < 0.5) {\n" + "    if (gl_PointCoord.y < 0.5) {\n" + "      gl_FragColor = v_modelToWorld[0];\n" + "    } else {\n" + "      gl_FragColor = v_modelToWorld[2];\n" + "    }\n" + "  } else {\n" + "    if (gl_PointCoord.y < 0.5) {\n" + "      gl_FragColor = v_modelToWorld[1];\n" + "    } else {\n" + "      gl_FragColor = v_modelToWorld[3];\n" + "    }\n" + "  }\n" + "}\n";
     var shaderProgram = createProgram(gl, vertexShader, fragmentShader);
-    var texture = gl.createTexture();
+    function setupTexture(width, values) {
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, 1, 0, gl.RGBA, gl.FLOAT, values);
+        return texture;
+    }
     gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    var data = new Float32Array([
+    setupTexture(1, new Float32Array([
         1.0,
         0.0,
         0.0,
-        1.0,
-        1.0,
-        1.0,
+        0.0,
+    ]));
+    gl.activeTexture(gl.TEXTURE1);
+    setupTexture(1, new Float32Array([
         0.0,
         1.0,
         0.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
+        0.0,
+    ]));
+    gl.activeTexture(gl.TEXTURE2);
+    setupTexture(1, new Float32Array([
+        0.0,
         0.0,
         1.0,
-        1.0
-    ]);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 2, 2, 0, gl.RGBA, gl.FLOAT, data);
+        0.0
+    ]));
+    gl.activeTexture(gl.TEXTURE3);
+    setupTexture(1, new Float32Array([
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+    ]));
+    gl.activeTexture(gl.TEXTURE4);
+    setupTexture(1, new Float32Array([
+        .5,
+        .25,
+        0.0,
+        0.0,
+    ]));
     gl.useProgram(shaderProgram);
-    var a_Position = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.enableVertexAttribArray(a_Position);
     var positionsBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1 + perPixelX, 0, 1 - perPixelX, 0]), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1 + perPixelX, 0]), gl.STATIC_DRAW);
+    var vertexCount = 1;
+    var a_Position = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.enableVertexAttribArray(a_Position);
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-    var vertexCount = 2;
-    var a_TexCoord = gl.getAttribLocation(shaderProgram, "a_TexCoord");
-    gl.enableVertexAttribArray(a_TexCoord);
     var texCoordsBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, texCoordsBuffer);
+    var a_TexCoord = gl.getAttribLocation(shaderProgram, "a_TexCoord");
+    gl.enableVertexAttribArray(a_TexCoord);
     gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, 0, 0);
-    var u_Sampler = gl.getUniformLocation(shaderProgram, "u_Sampler");
-    gl.uniform1i(u_Sampler, 0);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "u_parentMatrixA"), 0);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "u_parentMatrixB"), 1);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "u_parentMatrixC"), 2);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "u_parentMatrixD"), 3);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "u_fractionAndAlignment"), 4);
     function drawScene() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         var start = Date.now();
